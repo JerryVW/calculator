650dde93820fc3188fe1f91cde23cdd4188347aa
JerryVW <jerryvw72@gmail.com>
2020-03-17 11:03:33 -0400

    Got layout done an majority of functionality complete

--- script.js
+++ script.js
@@ -1,17 +1,62 @@
-let numb;
-let sum;
+let numb1;
+let numb2;
+let calcBtn;
 let total;
-const button = document.querySelector("#calc-body div").addEventListener("click", getUserInputs);
+let display = document.getElementById("output-display");
+const getNumButtons = document.querySelectorAll(".std-btn");
+const getOperatorButtons = document.querySelectorAll(".calc-btn");
+const clearButton = document.getElementById("clear-btn").addEventListener("click", clearEverything);
+const equalButton = document.getElementById("equalBtn").addEventListener("click", calculateTotal);
 
-function calculatorApp(e) {
-    getUserInputs();
-    clearEverything();
+getNumButtons.forEach(function (button) {
+    button.addEventListener("click", function(e) {
+        if(display.innerText === "" || display.innerText === calcBtn) {
+            display.innerText = e.target.value;
+        } else{
+            display.innerText = display.innerText + e.target.value; 
+        }
+    })
+});
+
+getOperatorButtons.forEach(function(opBtn) {
+    opBtn.addEventListener("click", function(e) {
+        if (e.target.className === "calc-btn") {
+            calcBtn = e.target.value;
+            display.innerText = calcBtn;
+        }
+    })
+});
+
+function setNumbers() {
+    let userValue = display.innerText;
+    if(calcBtn === undefined) {
+        numb1 = parseFloat(userValue);
+    } else if (numb1 !== null && calcBtn !== undefined) {
+        numb2 = parseFloat(userValue);
+    }
 }
 
-function getUserInputs(e) {
-    if(e.target.className === "std-btn") {
-        numb = parseInt(e.target.value);
-        document.getElementById("output-display").innerHTML = numb; 
+function calculateTotal() {
+    switch(getOperatorButtons) {
+        case "+":
+            total = numb1 + numb2;
+            display.innerText = total;
+            break;
+
+        case "-":
+            total = numb1 - numb2;
+            display.innerText = total;
+            break;
+
+        case "*":
+            total = numb1 * numb2;
+            display.innerText = total;
+            break;
+
+        case "/":
+            total = numb1 / numb2;
+            display.innerText = total;
+            break;
     }
 }
 
