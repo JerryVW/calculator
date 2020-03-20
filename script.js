fad0984d85ad3ec8edce982c4dd4b4782d22144e
JerryVW <jerryvw72@gmail.com>
2020-03-20 15:11:01 -0400

    Worked on trying to get calculation after another operator is selected

--- script.js
+++ script.js
@@ -3,41 +3,43 @@ let numb2;
 let calcBtn;
 let total;
 let display = document.getElementById("output-display");
+let decimalButton = document.getElementById("decimal-btn");
 const getNumButtons = document.querySelectorAll(".std-btn");
-const decimalButton = document.getElementById("decimal-btn");
+const setOperatorButtons = document.querySelectorAll(".calc-btn");
 const setNumbers = document.querySelectorAll(".std-btn");
-const getOperatorButtons = document.querySelectorAll(".calc-btn");
 const clearButton = document.getElementById("clear-btn").addEventListener("click", clearEverything);
 const equalButton = document.getElementById("equalBtn").addEventListener("click", calculateTotal);
 
 getNumButtons.forEach(function (button) {
     button.addEventListener("click", function(e) {
-        if(display.innerText === "" || display.innerText === calcBtn) {
-            display.innerText = e.target.value;   
+        if(display.value === "" || display.value === calcBtn) {
+            display.value = e.target.value;   
         } else if(e.target.value === ".") {
-            display.innerText += e.target.value;
+            display.value += e.target.value;
             decimalButton.value = ""; 
         }else {
-            display.innerText += e.target.value; 
+            display.value += e.target.value; 
         }
     })
 });
 
-getOperatorButtons.forEach(function(opBtn) {
+setOperatorButtons.forEach(function(opBtn) {
     opBtn.addEventListener("click", function(e) {
         if (e.target.className === "calc-btn") {
             calcBtn = e.target.value;
-            display.innerText = calcBtn;
-            if(display.innerText === calcBtn) {
+            display.value = calcBtn;
+            if(display.value === calcBtn) {
                 decimalButton.value = ".";
             }
+        } else if(numb1 !== null && numb2 !== undefined && calcBtn !== undefined) {
+            calculateTotal(numb1, numb2, calcBtn);
         }
     })
 });
 
 setNumbers.forEach(function (e) {
     e.addEventListener("click", function() {
-        let userValue = display.innerText;
+        let userValue = display.value;
         if(calcBtn === undefined) {
             numb1 = parseFloat(userValue);
         } else if (numb1 !== null && calcBtn !== undefined) {
@@ -47,39 +49,37 @@ setNumbers.forEach(function (e) {
 }); 
   
 function calculateTotal(e) {
-    if(e.target.value === "=") {
-        decimalButton.value = ".";
-    }
     switch(calcBtn) {
         case "+":
             total = numb1 + numb2;
-            display.innerText = +total.toFixed(3);
+            display.value = +total.toFixed(3);
             numb1 = total;
             break;
 
         case "-":
             total = numb1 - numb2;
-            display.innerText = +total.toFixed(3);
+            display.value = +total.toFixed(3);
             numb1 = total;
             break;
 
         case "x":
             total = numb1 * numb2;
-            display.innerText = +total.toFixed(3);
+            display.value = +total.toFixed(3);
             numb1 = total;
             break;
 
         case "/":
             total = numb1 / numb2;
-            display.innerText = +total.toFixed(3);
+            display.value = +total.toFixed(3);
             numb1 = total;
             break;
     }
 }
 
 function clearEverything() {
-    document.getElementById("output-display").innerHTML = "";
+    display.value = "";
     numb1 = null;
     numb2 = null;
     calcBtn = undefined;
+    decimalButton.value = ".";
 }
