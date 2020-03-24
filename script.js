7ba0b4315373890e45533b491c12d127c4b0fcd5
JerryVW <jerryvw72@gmail.com>
2020-03-24 15:52:51 -0400

    Got calculations to work, but have issue with decimals

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let numb1;
 let numb2;
+let result;
 let calcBtn;
 let total;
 let display = document.getElementById("output-display");
@@ -40,12 +41,16 @@ function getNumber(target) {
 function getDecimal(decimal) {
     if(decimal === ".") {
         display.value += decimal;
+    } else if(display.value = calcBtn) {
+        display.value = decimal;
     }
 }
 
 function getOperator(target) {
     if(numb1 !== null && numb2 !== null && calcBtn !== undefined) {
         calculateTotal();
+        calcBtn = target.value;
+        display.value = calcBtn;
     } else if (target.className === "calc-btn") {
         calcBtn = target.value;
         display.value = calcBtn;
@@ -54,39 +59,40 @@ function getOperator(target) {
 
 function setNumbers(display) {
     let userValue = display;
-    if(calcBtn === undefined) {
+    if(result !== undefined) {
+        numb1 = result;
+        numb2 = parseFloat(userValue);
+    } else if(calcBtn === undefined) {
         numb1 = parseFloat(userValue);
-        console.log(numb1);
     } else if (numb1 !== null && calcBtn !== undefined) {
         numb2 = parseFloat(userValue);
-        console.log(numb2);
     }
 }
   
-function calculateTotal(e) {
+function calculateTotal() {
     switch(calcBtn) {
         case "+":
             total = numb1 + numb2;
             display.value = +total.toFixed(3);
-            numb1 = total;
+            result = total;
             break;
 
         case "-":
             total = numb1 - numb2;
             display.value = +total.toFixed(3);
-            numb1 = total;
+            result = total;
             break;
 
         case "x":
             total = numb1 * numb2;
             display.value = +total.toFixed(3);
-            numb1 = total;
+            result = total;
             break;
 
         case "/":
             total = numb1 / numb2;
             display.value = +total.toFixed(3);
-            numb1 = total;
+            result = total;
             break;
     }
 }
@@ -95,5 +101,6 @@ function clearEverything() {
     display.value = "";
     numb1 = null;
     numb2 = null;
-    calcBtn =undefined;
+    result = undefined;
+    calcBtn = undefined;
 }
